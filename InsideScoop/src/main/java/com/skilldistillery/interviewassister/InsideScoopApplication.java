c897790157ff5971da7a649d7be88077f01b864c
Ryan <snapsglasen@gmail.com>
2022-08-19 02:59:04 +0900

    created boot mvc project, link jpa project dependency

---
+++ InsideScoop/src/main/java/com/skilldistillery/interviewassister/InsideScoopApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.interviewassister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideScoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideScoopApplication.class, args);
+	}
+
+}
---
+++ InsideScoop/src/test/java/com/skilldistillery/interviewassister/InsideScoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.interviewassister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsideScoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
