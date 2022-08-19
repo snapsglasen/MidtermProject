baa68a0a118fe28e1a4f991ea38975efda3d950b
Ryan <snapsglasen@gmail.com>
2022-08-19 22:53:26 +0900

    tested post entity

---
+++ JPAPopeyesDroolFree/src/test/java/com/skilldistillery/interviewassister/entities/PostTest.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.interviewassister.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Post post;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("InsideScoop");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		post = em.find(Post.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		post = null;
+	}
+
+	@Test
+	void test_Post_not_null() {
+		assertNotNull(post);
+	}
+	
+	@Test
+	void test_Post_mappings() {
+		assertEquals("The title of a post", post.getTitle());
+		
+	}
+
+}
